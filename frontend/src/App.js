f1ba5abf1db343c2dc356e86a429d05836f8633d
Melih <mlhdzdr@gmail.com>
2021-09-27 21:17:19 +0300

    place order sayfası tamamlandı

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import { signout } from "./actions/userActions";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 import PaymentMethodScreen from "./screens/PaymentMethodScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 
 function App() {
   //Değişkenleri tanımladığımız kod satırı.
@@ -59,6 +60,7 @@ function App() {
             <Route path="/register" component={RegisterScreen}/>
             <Route path="/shipping" component={ShippingAddressScreen}/>
             <Route path="/payment" component={PaymentMethodScreen}/>
+            <Route path="/placeorder" component={PlaceOrderScreen}/>
             <Route path="/" component={HomeScreen} exact/>
           </main>
           <footer className="row center">All right reserved</footer>
