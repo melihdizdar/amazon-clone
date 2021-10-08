73c5383d64c266b897ba99358956f6d4a4521f04
Melih <mlhdzdr@gmail.com>
2021-10-09 02:08:27 +0300

    force order items from one seller

--- frontend/src/actions/cardActions.js
+++ frontend/src/actions/cardActions.js
@@ -1,12 +1,34 @@
 import axios from 'axios';
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_ADD_ITEM_FAIL, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
 
 export const addToCart = (productId,qty) => async(dispatch,getState) =>{
     /*burda sepete ürün eklediğimiz zaman data.js den hangi verilerin çekileceğini tanımlayıp 
     linkleme işleminin hangi idden çekileceğini ayarladık.*/
     /*Sepete ürün aktarma aksiyonu*/
     const {data} = await axios.get(`/api/products/${productId}`);
-    dispatch({
+    const {cart:{cartItems},} = getState(); //52.Force order items from one seller
+    if (cartItems.length > 0 && data.seller._id !== cartItems[0].seller._id) { //52.Force order items from one seller
+        dispatch({ //52.Force order items from one seller
+            type: CART_ADD_ITEM_FAIL, //52.Force order items from one seller
+            payload: `Can't Add To Cart. Buy only from ${cartItems[0].seller.seller.name} in this order`, //52.Force order items from one seller
+        });
+    }
+    else{
+        dispatch({ //52.Force order items from one seller
+        type: CART_ADD_ITEM, //52.Force order items from one seller
+        payload:{ //52.Force order items from one seller
+            name: data.name, //52.Force order items from one seller
+            image: data.image, //52.Force order items from one seller
+            price: data.price, //52.Force order items from one seller
+            coutInStock: data.coutInStock, //52.Force order items from one seller
+            product: data._id, //52.Force order items from one seller
+            seller: data.seller, //52.Force order items from one seller
+            qty, //52.Force order items from one seller
+        },
+    });
+    localStorage.setItem('cartItems',JSON.stringify(getState().cart.cartItems)); //52.Force order items from one seller
+    }
+    /*dispatch({
         type: CART_ADD_ITEM,
         payload:{
             name: data.name,
@@ -18,7 +40,7 @@ export const addToCart = (productId,qty) => async(dispatch,getState) =>{
             qty,
         },
     });
-    localStorage.setItem('cartItems',JSON.stringify(getState().cart.cartItems));
+    localStorage.setItem('cartItems',JSON.stringify(getState().cart.cartItems));*/
 };
 export const removeFromCart = (productId) => (dispatch,getState) => {
     /*burda sepetteki ürünün silinme aksiyonu gerçekleşiyor.*/
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_EMPTY, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_ADD_ITEM_FAIL, CART_EMPTY, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
 
 export const cartReducer = (state={cartItems:[]},action) =>{
     /*redux içine sepetteki itemlerin eklemesini ve silinesini ekledik.*/
@@ -9,10 +9,12 @@ export const cartReducer = (state={cartItems:[]},action) =>{
             if(existItem){
                 return{
                     ...state,
+                    error: '', //52.Force order items from one seller
                     cartItems: state.cartItems.map(x => x.product === existItem.product? item: x),
                 };
             } else{
-                return {...state,cartItems:[...state.cartItems,item]};
+                //return {...state,cartItems:[...state.cartItems,item]};
+                return {...state,error: '',cartItems:[...state.cartItems,item]}; //52.Force order items from one seller
             }
         case CART_REMOVE_ITEM:
             return {...state,cartItems: state.cartItems.filter(x => x.product !== action.payload),
@@ -21,8 +23,11 @@ export const cartReducer = (state={cartItems:[]},action) =>{
             return {...state, shippingAddress: action.payload};
         case CART_SAVE_PAYMENT_METHOD:
             return {...state, paymentMethod: action.payload};
+        case CART_ADD_ITEM_FAIL: //52.Force order items from one seller
+            return { ...state, error: action.payload }; //52.Force order items from one seller
         case CART_EMPTY:
-            return {...state, cartItems: [] };
+            //return {...state, cartItems: [] };
+            return { ...state, error: '', cartItems: [] }; //52.Force order items from one seller
     default:
         return state;
     }
